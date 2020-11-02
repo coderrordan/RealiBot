8e227343065f53bbeb4a434d911fdb6a54d15608
coderrordan <patriziodaniele.napolitano@gmail.com>
2020-11-02 23:31:30 +0100

    grosso update di prova

--- RealiBot.py
+++ RealiBot.py
@@ -11,31 +11,18 @@ async def callback(bot, update):
         text = update['message']['text']
 
         if text == '/start':
-            await bot.sendMessage(chat_id=chat_id, text='EzTGPy example bot\n\nCommands:\n/inline\n/keyboard\n/hidekb\n\nYour id: %s\nChat id: %s\nMessage id: %s' %
-                            (user_id, chat_id, message_id))  # you can find method parameters in https://core.telegram.org/bots/api#sendmessage
+            #menu nel messaggio
+            MenuStart()
 
-        if text == '/inline':
-            keyboard = EzTG.Keyboard('inline')
-            keyboard.add('Example', 'callback data')
-            keyboard.add('Example 2', 'callback data 2')
-            keyboard.newLine()
-            keyboard.add('Example 3', 'https://google.it')
-            await bot.sendMessage(chat_id=chat_id, text='Test',
-                            reply_markup=keyboard)
+            # you can find method parameters in https://core.telegram.org/bots/api#sendmessage
 
-        if text == '/keyboard':
-            keyboard = EzTG.Keyboard('keyboard')
-            keyboard.add('Example 1')
-            keyboard.add('Example 2')
-            keyboard.newLine()
-            keyboard.add('Example 3')
-            await bot.sendMessage(chat_id=chat_id, text='Test',
-                            reply_markup=keyboard)
 
-        if text == '/hidekb':
-            keyboard = EzTG.Keyboard('remove')
-            await bot.sendMessage(chat_id=chat_id,
-                            text='Adios keyboard', reply_markup=keyboard)
+        #nasconde il menu
+        if text == '/tastiera':
+            RimuoviMenu()
+            await bot.sendMessage(chat_id=chat_id, reply_markup=keyboard)
+
+
     if 'callback_query' in update:
         # callback query "handler"
         message_id = update['callback_query']['message']['message_id']
@@ -44,18 +31,79 @@ async def callback(bot, update):
         cb_id = update['callback_query']['id']
         cb_data = update['callback_query']['data']
 
-        if cb_data == 'callback data':
-            await bot.answerCallbackQuery(callback_query_id=cb_id, text='example #1')  # you can find method parameters in https://core.telegram.org/bots/api#answercallbackquery
-            keyboard = EzTG.Keyboard('inline')
-            keyboard.add('Example 2', 'callback data 2')
-            await bot.editMessageText(chat_id=chat_id, message_id=message_id,
-                                text='New message', reply_markup=keyboard)  # you can find method parameters in https://core.telegram.org/bots/api#editmessagetext
+        if cb_data == 'callback contatti':
+            MenuContatti()
+              # you can find method parameters in https://core.telegram.org/bots/api#editmessagetext
 
-        if cb_data == 'callback data 2':
-            await bot.answerCallbackQuery(
-                callback_query_id=cb_id, text='example #2 [alert]', show_alert=True)
-            await bot.editMessageText(chat_id=chat_id, message_id=message_id,
-                                text='New message 2', reply_markup={})
+        if cb_data == 'callback servizi':
+            MenuServizi()
+
+
+        if cb_data == 'callback indietro':
+            indietro('contatti')
+            await bot.editMessageText(chat_id=chat_id, message_id=message_id)
+
+
+def MenuPrincipale():
+    keyboard = EzTG.Keyboard('inline')
+    keyboard.add('Contatti', 'callback contatti')
+    keyboard.newLine()
+    keyboard.add('Servizi', 'callback servizi')
+
+def MenuSecondario():
+    menu = EzTG.Keyboard('keyboard')
+    menu.add('INDIETRO', 'indietro')
+    menu.add('HOME', 'home')
+    menu.newLine()
+    menu.add('ARRESTA BOT', 'stop')
+
+def MenuStart():
+    MenuPrincipale()
+    MenuSecondario()
+    await bot.sendMessage(chat_id=chat_id, text=ListaComandi(), reply_markup=keyboard)
+
+def ListaComandi():
+    command="""Ciao sono RealiBot!
+    Sono qui per mostrarti tutta la potenzialità di Lean Sales e perchè è la community italiana n. 1
+
+    Comandi:
+    /start
+    /menu
+    /tastiera
+    /stop
+    """
+    return command
+
+def RimuoviMenu():
+    menu = EzTG.Keyboard('keyboard')
+
+def MenuContatti():
+    keyboard = EzTG.Keyboard('inline')
+    keyboard.add('Simone Reali', 'callback simone')
+    menu.newLine()
+    keyboard.add('Commericalista', 'callback commercialista')
+    menu.newLine()
+    keyboard.add('Spedizionere', 'callback spedizionere')
+    menu.newLine()
+    keyboard.add('<- INDIETRO', 'callback indietro')
+    await bot.editMessageText(chat_id=chat_id, message_id=message_id,
+                        text='Ecco tutti i Contatti Lean Sales', reply_markup=keyboard)
+
+def MenuServizi():
+    keyboard = EzTG.Keyboard('inline')
+    keyboard.add('controllo qualita', 'callback qualita')
+    menu.newLine()
+    keyboard.add('competizione potenziale', 'callback potenzialita')
+    menu.newLine()
+    keyboard.add('<- INDIETRO', indietro('servizi'))
+    await bot.editMessageText(chat_id=chat_id, message_id=message_id,
+                        text='Ecco tutti i Servizi Lean Sales', reply_markup=keyboard)
+
+def indietro(source):
+    if(source=='contatti'):
+        MenuPrincipale()
+    elif(source=='servizi'):
+        MenuPrincipale()
 
 
 bot = EzTG.EzTG(token='1494645372:AAHlmQr4hWPd3HPf-00WzzCaz5FdksfCeH4',
